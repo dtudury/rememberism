c421b4b73cef7f43018ae7e57a5caaf37c0acbd9
David Tudury <david.tudury@gmail.com>
2019-09-04 10:59:09 -0700

    try to find android font

--- docs/components/sight-word.js
+++ docs/components/sight-word.js
@@ -33,7 +33,7 @@ export default class extends HTMLElement {
             font-size: 64px;
             padding: 32px 16px;
             margin: 0;
-            font-family: "Comic Sans MS", cursive, sans-serif;
+            font-family: casual, "comic sans ms", cursive;
           }
           hr {
             border: none;
